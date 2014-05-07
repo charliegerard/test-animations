f2fe23ace0e1d48a2ca1554e0b685ecc2bb3063d
Charlie <annecharlotte.gerard@gmail.com>
2014-05-08 09:50:53 +1000

    Device Orientation Game

---
+++ device-orientation/js/device-orientation.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+	var drawWhole = function(){
+		//Creates a new whole at random coordinates on the page on each page load.
+		$('#whole').css({
+			left: Math.floor(Math.random() * (window.innerWidth - 100)),
+			top: Math.floor(Math.random() * (window.innerHeight - 100))
+		})
+	}
+
+	drawWhole();
+
+	if(! window.DeviceOrientationEvent){
+		return;
+	}
+
+	$(window).on('deviceorientation', function(event){
+		//console.log(event.originalEvent.alpha, event.originalEvent.beta, event.originalEvent.gamma);
+		var drawCircle = function(){
+			$('#circle').css({
+				marginLeft: 10 * event.originalEvent.gamma,
+				marginTop: 10 * event.originalEvent.beta
+			})
+		}
+
+		drawCircle();
+		ballFall();
+	});
+
+	var counter = 0;
+	var score = $('#score').html(counter);
+
+	var ballFall = function(){
+		if((parseInt($('#circle')[0].style.marginLeft) >= parseInt($('#whole')[0].offsetLeft) && parseInt($('#circle')[0].style.marginLeft) <= parseInt($('#whole')[0].offsetLeft) + 80) && (parseInt($('#circle')[0].style.marginTop) >= parseInt($('#whole')[0].offsetTop) && (parseInt($('#circle')[0].style.marginTop) <= parseInt($('#whole')[0].offsetTop) + 80))){
+		 	drawWhole();
+		 	counter++;
+		 	var score = $('#score').html(counter);
+		}
+	}
+
+	if(parseInt($('#score').html()) == 1){
+	 	$('#platform').html("Yeah")
+	 	console.log("Yeah")
+	}
+
+});
