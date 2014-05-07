652435f8dbc5c5ef6db90394dd7eec13bbd631eb
Charlie <annecharlotte.gerard@gmail.com>
2014-05-07 21:49:14 +1000

    Cats falling randomly at random speed

--- Shooting_cats/js/shooting_cats.js
+++ Shooting_cats/js/shooting_cats.js
@@ -4,21 +4,27 @@ $(document).ready(function(){
 	//Sound
 	var catSound = new Audio("http://www.kessels.com/CatSounds/kitten4.wav");
 
-	for(i = 0; i <= 7; i++){
-		//Create x number of cats 
-		var newCats = document.createElement("img");
-			newCats.setAttribute("src", "http://media.giphy.com/media/67j0Iy0UzvBG8/giphy.gif")
-			$(newCats).addClass('dancingCats');
-			newCats.id = ('newCat' + i)
-			document.getElementById("cats").appendChild(newCats)
+	var makeCats = function () {
+		for(i = 0; i <= 7; i++){
+			//Create x number of cats 
+			var newCats = document.createElement("img");
+				newCats.setAttribute("src", "http://media.giphy.com/media/67j0Iy0UzvBG8/giphy.gif")
+				$(newCats).addClass('dancingCats');
+				newCats.id = ('newCat' + i)
+				document.getElementById("cats").appendChild(newCats)
 
-			//Access left coordinate of each cat.
-			newCats.style.left = (120 * (i+1)) + 'px';
+				//Access left coordinate of each cat.
+				newCats.style.left = (120 * (i+1)) + 'px';
 
-			//Helps access each cat in the cats div
-			// Store an array of cat objects, where each has the cat image as a jquery object and a speed.
-			var dancingCatArray = $('.dancingCats')
-	}
+				//Helps access each cat in the cats div
+				// Store an array of cat objects, where each has the cat image as a jquery object and a speed.
+				var dancingCatArray = $('.dancingCats')
+				var catsArray = []
+				catsArray.push(newCats)
+				//console.log(catsArray[0])
+		}
+	};
+	makeCats();
 
 	$(window).mousemove(function(){
 		var e = window.event;
@@ -42,7 +48,7 @@ $(document).ready(function(){
 		}
 	});
 
- //Laser starts when the user clicks on the cat
+ 	//Laser starts when the user clicks on the cat
 	$('#shooting_cat').click(function(){
 	 	var e = window.event;
 	 	var newPosX = (e.clientX);
@@ -59,7 +65,12 @@ $(document).ready(function(){
 		 	$(newLaser).addClass('active');
 		 }, 100);
 
-	 	for(i = 0; i <= 4; i++){
+	 	//Trying to make the cats fall on the screen but doesnt work
+		// 	setTimeout(function () {
+		// 	 $(dancingCatArray).addClass('active');
+		// }, 1000);
+
+	 	for(i = 0; i <= 7; i++){
 	 		if (hitsCat(i, newPosX)) {
 	 			setTimeout(function () {
 		 			$(dancingCatArray[i]).hide();
@@ -87,5 +98,57 @@ $(document).ready(function(){
 		}
 	}
 
+	window.onload = function animateCats(){
+		//for(var x=0; x<=7; x++){
+			//$('.dancingCats')[0].style.top = '0px'
+			$('.dancingCats').css('top', 0).addClass('animated');
+			$('.dancingCats').each(function () {
+				// Save a random speed for each cat.
+				var speed = 50 + (Math.random() * 100);
+				$(this).data('speed', speed);
+			});
+			var fallingCats = function(){
+			//Makes all the cats move down at the same time
+				var test = function(){
+					var movingDown = parseInt($('.dancingCats').css('top'));
+					// var newPosition = movingDown + 100;
+					// $('.dancingCats') = newPosition + 'px'
+					//$('.dancingCats').css('top', '+=100');
+					$('.dancingCats').each(function () {
+						$(this).css('top', '+=' + $(this).data('speed'));
+					});
+					  if(movingDown > (window.innerHeight * 1.5)){
+					     	//clearInterval(fallingCatsTimer)
+					    $('.dancingCats').remove();
+						makeCats();
+						animateCats();
+							// $('.dancingCats').removeClass('animated').css('top', -300);
+							// setTimeout(function () {
+							// 	$('.dancingCats').addClass('animated');
+							// }, 200);
+					  }
+
+					// $.each(dancingCatArray, function(){
+					// 	//console.log(this)
+					// 	//Make the cats move down
+					// 	var movingDown = parseInt(this.style.top);
+					// 	var newPosition = movingDown + 100;
+					// 	this.style.top = newPosition + 'px'
+
+					// 	  if(movingDown > window.innerHeight){
+					// 	     	//clearInterval(fallingCatsTimer)
+					// 			$('.dancingCats').removeClass('animated').css('top', -300);
+					// 			setTimeout(function () {
+					// 				$('.dancingCats').addClass('animated');
+					// 			}, 200);
+					// 	  }
+					// });
+				}
+				var fallingCatsTimer = window.setInterval(test, 700)
+			}
+		//}
+	return[fallingCats()];
+	};
+	
 }); //End of document ready
 
