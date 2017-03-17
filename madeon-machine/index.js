88a4f18fa5c1ce0ec5d30ea5e011f8d355acc44f
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-03-17 15:27:38 +0000

    fix style when only one sound goes from active to inactive

--- madeon-machine/index.js
+++ madeon-machine/index.js
@@ -50,6 +50,9 @@ function start(){
       durationLoop = howls[sounds[blockId]].duration(blockId);
     }
 
+    // If the sound clicked is already playing, move it from the active array to the inactive one.
+    // And apply the pending style.
+    // Otherwise if it's inactive, move it to the active array.
     if(activeSounds.includes(soundObjects[blockId].soundNode)){
       inactiveSounds.push(soundObjects[blockId].soundNode);
       activeSounds.splice(elementIndex, 1);
@@ -64,7 +67,9 @@ function start(){
       }
     }
 
+    // If there is at least 1 sound and it is currently playing.
     if(activeSounds[0] && activeSounds[0].playing()){
+      //When the first sound clicked finishes, check the active and inactive arrays.
       activeSounds[0].on('end', function(){
         for(var x = 0; x < inactiveSounds.length; x++){
           inactiveSounds[x].stop();
@@ -81,7 +86,6 @@ function start(){
           soundObjects.filter(function(e){
             if(e.soundNode === activeSounds[i] && e.element.classList.contains('pending')){
               e.element.classList.remove('pending');
-
             }
 
             if(e.soundNode === activeSounds[i] && !e.element.classList.contains('active')){
@@ -89,24 +93,31 @@ function start(){
             }
           });
 
-
           activeSounds[i].stop();
           activeSounds[i].play();
         }
       })
 
+      //If there is one sound added but it's not playing yet (when clicking on 1st sound when launching the app.)
     } else if (activeSounds[0] && !activeSounds[0].playing()) {
       activeSounds[0].play();
+      // Map the sound playing to the correct div.
       soundObjects.filter(function(e){
         if(e.soundNode === activeSounds[0] && !e.element.classList.contains('active')){
           e.element.classList.add('active');
-
         }
       });
     } else { // If all sounds removed, stop the ones playing.
       inactiveSounds[inactiveSounds.length-1].on('end', function(){
         for(var i = 0; i < inactiveSounds.length; i++){
           inactiveSounds[i].stop();
+
+          soundObjects.filter(function(e){
+            if(e.soundNode === inactiveSounds[i] && e.element.classList.contains('pending')){
+              e.element.classList.remove('pending');
+              e.element.classList.remove('active');
+            }
+          });
         }
       })
     }
