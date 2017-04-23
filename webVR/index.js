c8dcf87619451bd9442e9611bbd398c5d55c59dd
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-04-23 13:58:30 +0100

    add scene with mountains and wally

--- webVR/index.js
+++ webVR/index.js
@@ -1,5 +1,12 @@
-var box = document.querySelector('a-box');
-box.getAttribute('position');
-box.addEventListener('click', function () {
-  box.setAttribute('color', 'red');
-});
+// var box = document.querySelector('a-box');
+// box.getAttribute('position');
+// box.addEventListener('click', function () {
+//   box.setAttribute('color', 'red');
+// });
+
+var entity = document.querySelector('a-entity');
+var model = document.getElementById('tree-obj');
+model.addEventListener('model-loaded', function(){
+  console.log('boo');
+  // model.setAttribute('obj-model', "obj: #tree-obj; mtl: #tree-mtl")
+})
